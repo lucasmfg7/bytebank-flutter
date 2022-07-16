6e14296f670f06681d41c4c8678000e0cef68305
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-07-16 20:19:14 -0300

    Implementando o design do item de transferência

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,24 @@ void main() => runApp(
             child: Icon(Icons.add),
             onPressed: null,
           ),
+          body: Column(
+            children: [
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text('100.0'),
+                  subtitle: Text('1000'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text('200.0'),
+                  subtitle: Text('1000'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
