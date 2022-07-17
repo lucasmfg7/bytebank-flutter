7678ee637641bdd7f602959e0bc5a65f37a292a4
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-07-16 21:52:46 -0300

    Refatora o código

--- lib/main.dart
+++ lib/main.dart
@@ -3,31 +3,51 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-          appBar: AppBar(
-            title: Text('Transferências'),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: null,
-          ),
-          body: Column(
-            children: [
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text('100.0'),
-                  subtitle: Text('1000'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text('200.0'),
-                  subtitle: Text('1000'),
-                ),
-              ),
-            ],
-          ),
-        ),
+            appBar: AppBar(
+              title: const Text('Transferências'),
+            ),
+            floatingActionButton: const FloatingActionButton(
+              onPressed: null,
+              child: Icon(Icons.add),
+            ),
+            body: TransferList()),
       ),
     );
+
+class TransferList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      // ignore: prefer_const_literals_to_create_immutables
+      children: [
+        TransferItem(Transfer(100, 1000)),
+        TransferItem(Transfer(200, 2000)),
+        TransferItem(Transfer(300, 3000)),
+      ],
+    );
+  }
+}
+
+class TransferItem extends StatelessWidget {
+  final Transfer _transfer;
+
+  TransferItem(this._transfer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: const Icon(Icons.monetization_on),
+        title: Text(_transfer.value.toString()),
+        subtitle: Text(_transfer.accountNumber.toString()),
+      ),
+    );
+  }
+}
+
+class Transfer {
+  final double value;
+  final double accountNumber;
+
+  Transfer(this.value, this.accountNumber);
+}
